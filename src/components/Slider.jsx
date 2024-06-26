28bf5eef457db5fa7f538432c05c6850c26a3b27
basa738997 <100225179+basa738997@users.noreply.github.com>
2024-06-27 00:15:01 +0530

    banner work on mobile view

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Autoplay, Pagination, Scrollbar, A11y } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
+
 const Slider = () => {
     return (
         <div className=''>
@@ -20,22 +21,22 @@ const Slider = () => {
                 }}
             >
                 <SwiperSlide>
-                    <div className="w-full h-screen">
+                    <div className="w-full h-96 sm:h-[80vh]">
                         <img className='w-full h-full object-cover' src="https://images.unsplash.com/photo-1461354464878-ad92f492a5a0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div className="w-full h-screen">
+                    <div className="w-full h-96 sm:h-[80vh]">
                         <img className='w-full h-full object-cover' src="https://images.unsplash.com/photo-1709532390940-5c09fcdf098d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div className="w-full h-screen">
+                    <div className="w-full h-96 sm:h-[80vh]">
                         <img className='w-full h-full object-cover' src="https://images.unsplash.com/photo-1542838132-92c53300491e?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div className="w-full h-screen">
+                    <div className="w-full h-96 sm:h-[80vh]">
                         <img className='w-full h-full object-cover' src="https://images.unsplash.com/photo-1471193945509-9ad0617afabf?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
                     </div>
                 </SwiperSlide>
@@ -44,4 +45,4 @@ const Slider = () => {
     )
 }
 
-export default Slider
+export default Slider;
